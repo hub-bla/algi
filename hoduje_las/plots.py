8f15bab210944db03c4cbf205182702076b88540
hub-bla <hubertblaszczyk03@gmail.com>
2023-04-10 14:11:46 +0200

    plots

--- hoduje_las/plots.py
+++ hoduje_las/plots.py
@@ -2,18 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('./measured_balance.csv')
+data = pd.read_csv('./measured_inorder.csv')
 
 
 
 plt.figure()
 
-# plt.plot(data['Tworzenie drzewa AVL'], color='b')
-# plt.errorbar(data.index,data['Tworzenie drzewa AVL'], data['std_AVL'],marker='o', color='b')
-plt.plot(data['Balansowanie drzewa BST'], color='r')
-plt.errorbar(data.index,data['Balansowanie drzewa BST'], data['std_BST'],marker='o', color='r')
+plt.plot(data['Tworzenie drzewa AVL'], color='b')
+plt.errorbar(data.index,data['Tworzenie drzewa AVL'], data['std_AVL'],marker='o', color='b')
+plt.plot(data['Tworzenie drzewa BST'], color='r')
+plt.errorbar(data.index,data['Tworzenie drzewa BST'], data['std_BST'],marker='o', color='r')
 plt.xlabel("n")
 plt.ylabel("t [s]")
-plt.title("Czas balansowania drzewa")
+plt.title("Czas inorder")
 plt.legend(['BST'])
 plt.show()
